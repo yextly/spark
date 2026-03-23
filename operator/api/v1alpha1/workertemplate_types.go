820a48aec5eae41c31c5202adc089a6daee0d404
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2026-03-23 09:38:25 +0100

    Improve the skeleton

--- operator/api/v1alpha1/workertemplate_types.go
+++ operator/api/v1alpha1/workertemplate_types.go
@@ -36,8 +36,6 @@ type WorkerTemplateSpec struct {
 type WorkerTemplateStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-
-	JobInstanceId string `json:"jobInstanceId,omitempty"`
 }
 
 // +kubebuilder:object:root=true
--- operator/internal/controller/workerinstance_controller.go
+++ operator/internal/controller/workerinstance_controller.go
@@ -19,9 +19,13 @@ package controller
 import (
 	"context"
 
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/api/errors"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	computev1alpha1 "spark/api/v1alpha1"
@@ -32,6 +36,8 @@ type WorkerInstanceReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
+
+const finalizerName = "workerinstance.compute.yextly.io"
 
 // +kubebuilder:rbac:groups=compute.yextly.io,resources=workerinstances,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=compute.yextly.io,resources=workerinstances/status,verbs=get;update;patch
@@ -47,9 +53,72 @@ type WorkerInstanceReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/reconcile
 func (r *WorkerInstanceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = logf.FromContext(ctx)
+	logger := logf.FromContext(ctx)
 
-	// TODO(user): your logic here
+	logger.Info(">>> Reconciliation", "namespace", req.Namespace, "name", req.Name)
+
+	instance := &computev1alpha1.WorkerInstance{}
+	// Retrieve the Ec2Instance resource from the Kubernetes API server using the provided request's NamespacedName.
+	if err := r.Get(ctx, req.NamespacedName, instance); err != nil {
+		if errors.IsNotFound(err) {
+			logger.Info("The instance has been deleted")
+
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	// Check if we are deleting
+	if !instance.DeletionTimestamp.IsZero() {
+		logger.Info("The instance is being deleted")
+		// _, err := deleteInstance(ctx, instance)
+		// if err != nil {
+		// 	l.Error(err, "Failed to delete instance")
+		// 	// Kubernetes will retry with backoff
+		// 	return ctrl.Result{Requeue: true}, err
+		// }
+
+		// Remove the finalizer
+		controllerutil.RemoveFinalizer(instance, finalizerName)
+		if err := r.Update(ctx, instance); err != nil {
+			logger.Error(err, "Failed to remove finalizer")
+
+			// Kubernetes will retry with backoff
+			return ctrl.Result{}, err
+		}
+		// At this point, the instance state is terminated and the finalizer is removed
+		return ctrl.Result{}, nil
+	}
+
+	if instance.Status.JobInstanceId == "" {
+		logger.Info("Add finalizer")
+		instance.Finalizers = append(instance.Finalizers, finalizerName)
+		if err := r.Update(ctx, instance); err != nil {
+			logger.Error(err, "Failed to add the finalizer")
+
+			return ctrl.Result{}, err
+		}
+
+		jobInstance, err := scheduleInstance(instance)
+		if err != nil {
+			logger.Error(err, "Failed to schedule the instance")
+
+			return ctrl.Result{}, err
+		}
+
+		instance.Status.JobInstanceId = jobInstance.Name
+
+		err = r.Status().Update(ctx, instance)
+		if err != nil {
+			logger.Error(err, "Failed to update the status")
+
+			return ctrl.Result{}, err
+		}
+
+	} else {
+		logger.Info("The resource is already bound to a Job", "jobInstanceId", instance.Status.JobInstanceId)
+		return ctrl.Result{}, nil
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -60,4 +129,8 @@ func (r *WorkerInstanceReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		For(&computev1alpha1.WorkerInstance{}).
 		Named("workerinstance").
 		Complete(r)
+}
+
+func scheduleInstance(logger *logr.Logger, template *computev1alpha1.WorkerTemplate, instance *computev1alpha1.WorkerInstance) (jobInstance *batchv1.Job, err error) {
+
 }
