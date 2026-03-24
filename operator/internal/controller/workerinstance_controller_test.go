4baf598a7d4b0a96d5e826f1575683f5f8538071
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2026-03-24 17:30:07 +0100

    Fix broken tests

--- operator/internal/controller/workerinstance_controller_test.go
+++ operator/internal/controller/workerinstance_controller_test.go
@@ -29,6 +29,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/record"
 
 	computev1alpha1 "spark/api/v1alpha1"
 
@@ -122,8 +123,9 @@ var _ = Describe("WorkerInstance Controller", func() {
 			By("Reconciling the created resource")
 
 			controllerReconciler := &WorkerInstanceReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
+				Client:        k8sClient,
+				Scheme:        k8sClient.Scheme(),
+				EventRecorder: record.NewFakeRecorder(100),
 			}
 
 			// --- Call reconcile  ---
